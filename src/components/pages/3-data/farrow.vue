8ca51b5cc98d1c991fcab63068b0337499b9430b
nohair-coder <691573104@qq.com>
2020-07-17 10:45:46 +0800

    7.17

--- src/components/pages/3-data/farrow.vue
+++ src/components/pages/3-data/farrow.vue
@@ -1,29 +1,24 @@
 <template>
   <div>
-    <bread bigtitle="母猪管理" smalltitle="生产信息" icon="el-icon-s-data"></bread>
+    <bread bigtitle="母猪管理" smalltitle="分娩信息录入" icon="el-icon-s-data"></bread>
     <div class="container">
       <div class="farrowitem">
         <div class="leftitem">
           <div class="1"
                style="width: 300px;height: 100px;">
-            <el-select
-              v-model="pig_earid"
-              style="margin-left: 20px;"
-              clearable
-              placeholder="请选择耳标号">
-              <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
+            <el-input
+              placeholder="请输入耳标号"
+              v-model.number="earid"
+              maxlength="8"
+              show-word-limit
+            >
+            </el-input>
           </div>
           <div style="width: 300px;height: 100px;margin-left: 20px;">疫病：无</div>
           <div style="width: 300px;height: 100px;margin-left: 20px;">体况：良好</div>
           <div style="width: 300px;height: 100px;margin-left: 20px;">配种周：40</div>
           <div style="width: 300px;height: 100px;margin-left: 20px;">
-            <el-button type="primary">录入生产信息</el-button>
+            <el-button type="primary" @click="add_farrow">录入生产信息</el-button>
           </div>
         </div>
         <div class="rightitem">
@@ -32,12 +27,31 @@
               <td class="column">{{item.key}}</td>
               <td class="column">{{item.value}}</td>
               <td class="column">{{item.key1}}</td>
-              <td class="column">{{item.value1}}</td>
             </tr>
           </table>
         </div>
       </div>
     </div>
+    <el-dialog title="录入生产信息" :visible.sync="dialogFormVisible" width="500px">
+      <el-form :model="form">
+        <el-form-item label="总产仔数 ">
+          <el-input v-model="form.total" type="number"></el-input>
+        </el-form-item>
+        <el-form-item label="产活仔数 ">
+          <el-input v-model="form.live" type="number"></el-input>
+        </el-form-item>
+        <el-form-item label="仔猪个体重">
+          <el-input v-model="form.heavy"></el-input>
+        </el-form-item>
+        <el-form-item label="窝重   ">
+          <el-input v-model="form.totalheavy"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="close">取 消</el-button>
+        <el-button type="primary" @click="add">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -51,82 +65,57 @@ export default {
   },
   data () {
     return {
+      earid: '',
       options: [],
+      dialogFormVisible: false,
+      form: [],
       pig_earid: '',
       statDatas: [
         {
-          key: '胎次',
-          value: '1',
-          key1: '2',
-          value1: '平均胎'
-        },
-        {
           key: '分娩日期',
-          value: '2015-1-15',
-          key1: '2015-2-3',
-          value1: '-'
+          value: '2020-7-15',
+          key1: '2019-7-15'
         },
         {
-          key: '断奶日期',
-          value: '2015-5-20',
-          key1: '2016-3-12',
-          value1: '-'
-        },
-        {
-          key: '第一次配种',
-          value: '2015-2-20',
-          key1: '2016-4-18',
-          value1: '-'
-        },
-        {
-          key: '最后一次配种',
-          value: '2015-2-28',
-          key1: '2016-4-1',
-          value1: '-'
-        },
-        {
-          key: '配种次数',
-          value: '3',
-          key1: '2',
-          value1: '2.5'
-        },
-        {
-          key: '怀孕次数',
-          value: '121',
-          key1: '115',
-          value1: '118'
-        },
-        {
-          key: '产仔量',
+          key: '总产仔数',
           value: '7',
-          key1: '7',
-          value1: '7'
+          key1: '8'
         },
         {
-          key: '死胎数',
-          value: '0',
-          key1: '0',
-          value1: '0'
-        },
-        {
-          key: '断奶数量',
+          key: '产活仔数',
           value: '7',
-          key1: '7',
-          value1: '7'
+          key1: '7'
         },
         {
-          key: '断奶前死亡',
-          value: '0',
-          key1: '0',
-          value1: '0'
+          key: '仔猪个体重',
+          value: '3,5,7',
+          key1: '6,6,7'
         },
         {
-          key: '成活率',
-          value: '100%',
-          key1: '100%',
-          value1: '100%'
+          key: '窝重',
+          value: '3,5,7',
+          key1: '6,6,7'
         }
       ]
+    }
+  },
+  methods: {
+    add_farrow () {
+      this.dialogFormVisible = true
+    },
+    close () {
+      this.dialogFormVisible = false
+      this.$message({
+        type: 'info',
+        message: '已取消录入'
+      })
+    },
+    add () {
+      this.dialogFormVisible = false
+      this.$message({
+        type: 'success',
+        message: '录入成功'
+      })
     }
   }
 }
@@ -163,5 +152,8 @@ export default {
     border: 1px solid #cad9ea;
     color: #666;
     height: 45px;
+  }
+  .el-input {
+    width: 210px;
   }
 </style>
